9b96bc83ef925cbceb00a46b19bfb85a541ceb51
domuz86 <59613646+domuz86@users.noreply.github.com>
2022-04-14 09:04:24 +0200

    Update index.js

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,5 +1,14 @@
-const BAKING_TEMPERATURES = {
-  cake: {low:180, high:250},
-  bread:{low:150, high:220},
-  pie:{low:200, high:230}
+const MY_CAR = {
+  wheels: 4,
+  cylinders: 4,
+  size: 2.2,
+};
+function main(myCar) {
+  // Only change code below this line
+  const { cylinders, size } = myCar;
+
+  // Only change code above this line
+  return { cylinders, size };
 }
+console.log(main(MY_CAR));
+module.exports = main;
